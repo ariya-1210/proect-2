cade046ec2e15ea374ae820df836f6f57013335e
ariya-1210 <ariyailiyahad@gmail.com>
2023-05-21 10:57:44 +0500

    figma-1

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,19 +1,25 @@
 import React from 'react'
-import { SlBasket } from 'react-icons/si';
+import { SlBasket } from 'react-icons/sl';
+import {AiOutlineSearch} from 'react-icons/ai';
 
 
 export const Navbar = () => {
   return (
     <div className='container'>
+      <div className="logo">
+        <h3>PORTEN</h3>
+      </div>
         <div className="box">
       <ul>
   <li><a>понравилось</a></li>
   <li><a>личный кабинет</a></li>
   <li><a>настройки</a></li>
   <li><a>
-  <SlBasket/>
+    <SlBasket/>
   </a></li>
-  <li><a></a></li>
+  <li><a>
+    <AiOutlineSearch/>
+  </a></li>
       </ul>
 
 
